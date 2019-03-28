efd75e12672f07f6ffec48f34f8111548847b632
baloghd <balogh.domonkos@gmail.com>
2019-03-28 21:27:00 +0100

    js magic

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 import pickle
+from typing import List
 
 from flask import Flask, render_template, request, redirect, flash, url_for
 from werkzeug.utils import secure_filename
 
 import face_recognition
+import pandas as pd
 
 ROOT_FOLDER = '/home/domi/PycharmProjects/baltaarc-flask/'
 UPLOAD_FOLDER = 'static/files/'
@@ -32,6 +34,12 @@ def get_similars(filename: str, howmany: int = 4):
     top_n_similar_files = lambda n: ["dataset/" + "_".join(image[0]) for image in s[:n]]
     return s, top_n_similar_files(howmany)
 
+def histogram(similarities: List, howmany: int = 20):
+    df = pd.DataFrame(similarities)
+    df[0] = df[0].apply(lambda x: " ".join(x[:-1]))
+    hist = df[0][:howmany].value_counts()
+    hist = list(zip(list(hist.index), map(lambda y: y * (100 / howmany), list(hist.values))))
+    return hist
 
 @app.route("/", methods = ['POST', 'GET'])
 def index():
@@ -48,7 +56,12 @@ def index():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             similarities, similar_files = get_similars(filename)
             jogalapok = [" ".join(s.split("/")[-1].split("_")[:-1]) for s in similar_files]
-            return render_template("base.html", file = filename, hasonlok = list(zip(similar_files, jogalapok)))
+
+
+            return render_template("base.html",
+                                   file = filename,
+                                   hasonlok = list(zip(similar_files, jogalapok)),
+                                   histogram = histogram(similarities, 20))
 
     return render_template("base.html")
 
---
+++ static/js/scripts.js
@@ -0,0 +1,6 @@
+function testHoldon(themeName){
+    HoldOn.open({
+        theme:themeName,
+        message:"<h4>"+ "Ítélethozatal folyamatban...</h4>"
+    });
+}
